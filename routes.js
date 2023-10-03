ed7f3db034d75f438c0b13f53c0f065d9339f454
soma <soma@koii.network>
2023-10-03 17:33:15 -0300

    update API

--- routes.js
+++ routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const keywords = require("./keywords");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Parse incoming request bodies in a middleware before your handlers
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 // Define a basic route
 app.get("/", (req, res) => {
@@ -10,10 +16,26 @@ app.get("/", (req, res) => {
 
 // Define the /keywords endpoint
 app.get("/keywords", (req, res) => {
+  console.log("Keyword request received from client with key:", req.query.key);
+
   const wordsList = require("./top1000words.json");
   const randomIndex = Math.floor(Math.random() * wordsList.length);
+  const keyword = wordsList[randomIndex];
+  console.log("Keyword sent to client:", keyword);
+  res.send(keyword);
+});
 
-  res.send(wordsList[randomIndex]);
+// Define the POST endpoint
+app.post("/get-secret", (req, res) => {
+  console.log("SecretKey request received from client", req.body);
+  const key = "GDZ9EGX1wVvELJrAviRVoj9VN1dNXmJYbb4qipczkuJC";
+  const expectedValue = 2000000000;
+
+  if (req.body[key] === expectedValue) {
+    res.json({ secretKey: process.env.SECRET_KEY });
+  } else {
+    res.status(400).json({ error: "Invalid data" });
+  }
 });
 
 app.listen(PORT, () => {
